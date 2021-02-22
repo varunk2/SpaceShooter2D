71b2497285e45f2333182c1215cac6ffe7d1f022
Varun Sharma <varunk22110@gmail.com>
2021-02-22 19:02:12 +0530

    Implemented player movement and movement bounds.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed;
+    public Rigidbody2D rigidBody;
+    public Transform bottomLeftLimit, topRightLimit;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector2 movementVector = new Vector2(horizontal, vertical);
+        rigidBody.velocity = movementVector * moveSpeed;
+
+        float movementLimitsX = Mathf.Clamp(transform.position.x,bottomLeftLimit.position.x, topRightLimit.position.x);
+        float movementLimitsY = Mathf.Clamp(transform.position.y, bottomLeftLimit.position.y, topRightLimit.position.y);
+        transform.position = new Vector3(movementLimitsX, movementLimitsY, transform.position.z);
+    }
+}
