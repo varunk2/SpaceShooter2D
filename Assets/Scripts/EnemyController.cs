41f76bf217d1f44fe7a9dcf4290f7b67fa177cc4
Varun Sharma <varunk22110@gmail.com>
2021-03-08 14:41:14 +0530

    Created Enemy movement and Enemy attack.

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -5,9 +5,35 @@ using UnityEngine;
 public class EnemyController : MonoBehaviour
 {
     public float moveSpeed;
+    public Vector2 startDirection;
+
+    public bool shouldChangeDirection;
+    public float changeDirectionXPoint;
+    public Vector2 changedDirection;
 
     void Update() {
-        transform.position -= new Vector3(moveSpeed * Time.deltaTime, 0f, 0f);
+        EnemyMovement();
+    }
+
+    private void EnemyMovement() {
+        //transform.position -= new Vector3(moveSpeed * Time.deltaTime, 0f, 0f);
+
+        if (!shouldChangeDirection) {
+            ChangeDirection(startDirection);
+        } else {
+            if(transform.position.x > changeDirectionXPoint) {
+                ChangeDirection(startDirection);
+            } else {
+                ChangeDirection(changedDirection);
+            }
+        }
+    }
+
+    private void ChangeDirection(Vector2 direction) {
+        float newMoveSpeedX = direction.x * moveSpeed * Time.deltaTime;
+        float newMoveSpeedY = direction.y * moveSpeed * Time.deltaTime;
+        float newMoveSpeedZ = 0f;
+        transform.position += new Vector3(newMoveSpeedX, newMoveSpeedY, newMoveSpeedZ);
     }
 
     private void OnBecameInvisible() {
---
+++ Assets/Scripts/EnemyShotsController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyShotsController : MonoBehaviour {
+    public float shotSpeed = 7f;
+    public GameObject impactEffect;
+
+    void Update() {
+        transform.position -= new Vector3(shotSpeed * Time.deltaTime, 0f, 0f);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other.gameObject.GetComponent<PlayerController>() != null) {
+            Instantiate(impactEffect, transform.position, Quaternion.identity);
+            HealthManagerController.instance.DamagePlayer();
+            Destroy(this.gameObject);
+        }
+    }
+
+    private void OnBecameInvisible() {
+        Destroy(gameObject);
+    }
+}
